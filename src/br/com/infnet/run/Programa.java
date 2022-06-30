e5488d8d97b478bcbd094a632c845848819fcb4a
marciofroes <lopes.froes@gmail.com>
2022-06-30 15:31:50 -0300

    exemplo com promocao
    promocao do cargo

--- src/br/com/infnet/run/Programa.java
+++ src/br/com/infnet/run/Programa.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import br.com.infnet.rh.model.Cargo;
 import br.com.infnet.rh.model.Funcionario;
+import br.com.infnet.rh.service.promocao.PromocaoService;
 import br.com.infnet.rh.service.reajuste.ReajusteService;
 import br.com.infnet.rh.service.reajuste.ValidacaoPercentualReajuste;
 import br.com.infnet.rh.service.reajuste.ValidacaoReajuste;
@@ -16,17 +17,24 @@ public class Programa {
 		Funcionario funcionario = new Funcionario("Tulio", "123123132", Cargo.ASSISTENTE, new BigDecimal("5.000"));
 		System.out.println("############################### Reajuste Salario ###############################");
 		System.out.println("Funcionario : " + funcionario.getNome());
+		System.out.println("Cargo : " + funcionario.getCargo());
 		System.out.println("Salario atual : " + funcionario.getSalario());
 
 		ValidacaoPercentualReajuste percentualReajuste = new ValidacaoPercentualReajuste();
 		List<ValidacaoReajuste> validacoes = new ArrayList<>();
 		validacoes.add(percentualReajuste);
-
+		PromocaoService promocaoService = new PromocaoService();
+		promocaoService.promover(funcionario, true);
+		
 		ReajusteService reajusteService = new ReajusteService(validacoes);
 		reajusteService.reajustarSalarioDoFuncionario(funcionario, new BigDecimal("0.20"));
 		System.out.println("############################### Reajuste Salario ###############################");
 		System.out.println("Funcionario : " + funcionario.getNome());
 		System.out.println("Salario ajustado : " + funcionario.getSalario());
+		
+		
+		System.out.println("Cargo : " + funcionario.getCargo());
+		
 	}
 
 }
